f54abf6b7e80a43231067077bd8bae4580f86a2c
Deepak3168 <ootaladeepak128@gmail.com>
2024-08-17 23:02:03 +0530

    added Docs

--- grey/settings.py
+++ grey/settings.py
@@ -25,10 +25,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['grey-scientific-labs-2.onrender.com']
+
+ALLOWED_HOSTS = ['grey-scientific-labs-2.onrender.com','0.0.0.0','127.0.0.1']
 
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
